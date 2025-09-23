de8fe3f1d88c9b6fb1e07c5289ad1a89f30e3ad8
ashwinbobby <ashwin.bobbyv@gmail.com>
2025-09-24 01:27:09 +0530

    changed CORs

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import io
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": ["https://client-flora-fresh.vercel.app"]}})
 
 # ================================
 # CONFIG
